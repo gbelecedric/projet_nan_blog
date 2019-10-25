53886499de36eaf267befe59a8f683f6b5350540
Opeyemi19 <47104102+Opeyemi19@users.noreply.github.com>
2019-10-25 13:01:01 +0000

    Modeif settings

--- nan_blog/nan_blog/settings.py
+++ nan_blog/nan_blog/settings.py
@@ -79,14 +79,20 @@ WSGI_APPLICATION = 'nan_blog.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'groupecedricdb',
-        'USER': 'groupecedric',
-        'PASSWORD': 'nan0980',
-        'HOST': '51.77.197.177',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'groupecedricdb',
+    #     'USER': 'groupecedric',
+    #     'PASSWORD': 'nan0980',
+    #     'HOST': '51.77.197.177',
+    #     'PORT': '5432',
+    # }
 }
 
 
